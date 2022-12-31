1d35205900a5fbe98df308adc28eb53ae1c3a1ef
Myrel666 <egaalmirajp@gmail.com>
2022-12-31 20:35:13 +0700

    V1

---
+++ resources/views/layouts/role/admin/master.blade.php
@@ -0,0 +1,19 @@
+
+  <!-- header -->
+  @section('title', 'Montern')
+  @include('layouts.role.admin.header')
+
+  <!-- Navbar -->
+  @include('layouts.role.admin.navbar')
+  
+
+  <!-- Main Sidebar Container -->
+  @include('layouts.role.admin.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  @include('layouts.role.admin.content')
+  <!-- /.content-wrapper -->
+  
+  <!-- footer -->
+  @include('layouts.role.admin.footer')
+
